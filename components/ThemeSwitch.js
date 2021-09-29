2c546150ab4a75cc40b5159a9102fca8d6acee53
fluidself <me@fluidself.org>
2021-09-29 12:45:00 +0200

    Replaces theme switch icon

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,21 +1,33 @@
-import { useColorMode, IconButton } from '@chakra-ui/react';
-import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+import { useColorMode, IconButton, Icon } from '@chakra-ui/react';
 
 export default function ThemeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
+
+  const iconTransform = {
+    light: 'rotate(0deg)',
+    dark: 'rotate(180deg)',
+  };
   const iconColor = {
-    light: 'black',
+    light: 'rgb(17, 17, 17)',
     dark: 'white',
   };
+
   return (
     <IconButton
-      aria-label="Toggle dark mode"
-      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
-      onClick={toggleColorMode}
       bg="transparent"
-      // w="100%"
-      // fontSize="20px"
-      // color={iconColor[colorMode]}
+      aria-label="Toggle dark mode"
+      onClick={toggleColorMode}
+      icon={
+        <Icon height="18px" width="18px" viewBox="0 0 18 18" fill="none" transition="0.4s" transform={iconTransform[colorMode]}>
+          <circle cx="9" cy="9" r="8.5" stroke={iconColor[colorMode]} />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9 18C13.9706 18 18 13.9706 18 9C18 4.02944 13.9706 0 9 0V18Z"
+            fill={iconColor[colorMode]}
+          />
+        </Icon>
+      }
     />
   );
 }
