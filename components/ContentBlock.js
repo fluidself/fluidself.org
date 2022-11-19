613bb7f396337a0e5f426dc072db8fadce6bc9ff
fluidself <me@fluidself.org>
2022-11-19 10:26:10 +0100

    Updates content block margins and borders

--- components/ContentBlock.js
+++ components/ContentBlock.js
@@ -2,8 +2,8 @@ import { Box, Heading, Stack } from '@chakra-ui/react';
 
 export default function ContentBlock({ heading, children }) {
   return (
-    <Stack border="3px solid" paddingBottom={2} marginBottom={5} mx={[2, 2, 0]}>
-      <Heading borderBottom="3px solid" px={3} py={1}>
+    <Stack border="solid" borderWidth={[2, 3, 3]} paddingBottom={2} marginBottom={5} mx={[0.5, 2, 0]}>
+      <Heading borderBottom="solid" borderBottomWidth={[2, 3, 3]} px={3} py={1}>
         {heading}
       </Heading>
       <Box px={3}>{children}</Box>
