e13a145dcf278b2d58270eea9895eb67cae12a2a
fluidself <me@fluidself.org>
2022-12-07 10:29:58 +0100

    Updates search box style

--- components/search/SearchBox.js
+++ components/search/SearchBox.js
@@ -1,15 +1,19 @@
 import { connectSearchBox } from 'react-instantsearch-dom';
-import { InputGroup, Input, InputRightElement } from '@chakra-ui/react';
+import { InputGroup, Input, InputRightElement, useColorModeValue } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
 function SearchBox({ refine }) {
+  const borderColor = useColorModeValue('blackAlpha.500', 'whiteAlpha.500');
+
   return (
-    <InputGroup maxW="325px" my={2}>
+    <InputGroup maxW="325px" mt={2} mb={4}>
       <Input
-        border="1px solid"
         type="search"
-        placeholder="Search for book or author..."
+        border="1px solid"
+        borderColor={borderColor}
         focusBorderColor="yellow.200"
+        placeholder="Search for book or author..."
+        _placeholder={{ opacity: 0.5, color: 'currentcolor' }}
         onChange={e => refine(e.currentTarget.value)}
       />
       <InputRightElement>
