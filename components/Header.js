1a22fd3e9328cb0dc5c25050d7cad1bc593784d9
fluidself <me@fluidself.org>
2023-01-16 08:06:33 +0100

    Replaces header icon and updates projects block

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import NextLink from 'next/link';
-import Image from 'next/image';
 import { Link, Flex, Stack } from '@chakra-ui/react';
 import ThemeSwitch from './ThemeSwitch';
-import pixelself from '../public/pixelself.png';
+import FSIcon from './FSIcon';
 
 export default function Header() {
   return (
-    <Flex as="nav" align="center" justify="space-between" wrap="wrap" w="100%" mb={4} mt={2} paddingLeft={3}>
+    <Flex as="nav" align="center" justify="space-between" wrap="wrap" w="100%" mb={4} mt={2}>
       <NextLink href="/">
-        <a style={{ height: '40px' }}>
-          <Image src={pixelself} alt="logo" width={40} height={40} />
+        <a>
+          <FSIcon />
         </a>
       </NextLink>
       <Stack spacing={6} align="center" direction="row">
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,37 @@
-import { Text, Link, Stack, UnorderedList, ListItem } from '@chakra-ui/react';
+import { Text, Link, Stack, UnorderedList, ListItem, Heading } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 import ContentBlock from '../components/ContentBlock';
 
 export default function Home() {
   return (
     <Layout>
-      <ContentBlock heading="DECK">
+      <ContentBlock heading="PROJECTS">
         <Stack>
+          <Heading size="lg">Dunlin</Heading>
           <Text>
-            DECK is an open-source notebook and knowledge graph that helps individuals, DAOs, and other communities capture,
-            organize, make sense of, and share complex information. All notes are encrypted and can only by decrypted by the
-            intended audience.
+            Dunlin is an open source notebook and knowledge graph that helps individuals, DAOs, and other communities
+            capture, organize, make sense of, and share complex information. All notes are encrypted and can only by
+            decrypted by the intended audience.
           </Text>
           <Text>
-            Individuals can use DECK to take notes, journal, and compile their ideas, thoughts, and research in an interconnected
-            context.
+            Individuals can use Dunlin to take notes, journal, and compile their ideas, thoughts, and research in an
+            interconnected context.
           </Text>
           <Text>
-            DAOs and other communities can carry out collaborative research, manage projects, and create knowledge bases, FAQs,
-            and guides. Access to workspaces can be granted using tokens, NFTs, and blockchain identity as keys.
+            DAOs and other communities can manage projects, carry out collaborative research, and create knowledge
+            bases, FAQs, and guides. Access to workspaces can be granted using tokens, NFTs, and blockchain identity as
+            keys.
           </Text>
         </Stack>
         <UnorderedList mt={8} spacing={1.5}>
           <ListItem>
-            <Link isExternal href="https://usedeck.vercel.app">
-              usedeck.vercel.app
+            <Link isExternal href="https://dunlin.xyz">
+              dunlin.xyz
             </Link>
           </ListItem>
           <ListItem>
-            <Link isExternal href="https://github.com/fluidself/deck">
-              github.com/fluidself/deck
+            <Link isExternal href="https://github.com/fluidself/dunlin">
+              github.com/fluidself/dunlin
             </Link>
           </ListItem>
         </UnorderedList>
