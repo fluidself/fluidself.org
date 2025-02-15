772348f9e5005802589ce40db1c0312f886e7e03
fluidself <me@fluidself.org>
2025-02-15 10:43:49 +0100

    Renders semantically related books below book notes

--- pages/books/[category]/[slug].js
+++ pages/books/[category]/[slug].js
@@ -1,13 +1,16 @@
 import Head from 'next/head';
+import NextLink from 'next/link';
+import { Heading, Link, ListItem, Stack, UnorderedList } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
+import { neon } from '@neondatabase/serverless';
 import Layout from '../../../components/Layout';
 import ContentBlock from '../../../components/ContentBlock';
 import { getBookData, getAllCategories, getBooksInCategory } from '../../../lib/api';
 import { customRenderers } from '../../../lib/markdown-renderer';
 
 export default function Book(props) {
-  const { title, content, category, slug } = props;
+  const { title, content, category, slug, related } = props;
 
   return (
     <Layout>
@@ -20,6 +23,24 @@ export default function Book(props) {
           {content}
         </ReactMarkdown>
       </ContentBlock>
+
+      <Stack px={3} marginBottom={5}>
+        <Heading as="h3" size="md">
+          You might also enjoy
+        </Heading>
+        <UnorderedList pl={4}>
+          {related.map(book => {
+            const [category, slug] = book.url.split('/').slice(-2);
+            return (
+              <ListItem key={book.url}>
+                <NextLink href={`/books/${category}/${slug}`} passHref>
+                  <Link>{book.title}</Link>
+                </NextLink>
+              </ListItem>
+            );
+          })}
+        </UnorderedList>
+      </Stack>
     </Layout>
   );
 }
@@ -27,6 +48,10 @@ export default function Book(props) {
 export async function getStaticProps(context) {
   const { category, slug } = context.params;
   const bookData = getBookData(category, slug);
+  const sql = neon(process.env.DATABASE_URL);
+  const url = `${process.env.BASE_URL}/books/${category}/${slug}`;
+  const relatedBooks =
+    await sql`SELECT url, title from book_embeddings WHERE url <> ${url} ORDER BY embedding <-> (SELECT embedding FROM book_embeddings WHERE url = ${url} limit 1) asc LIMIT 10`;
 
   return {
     props: {
@@ -34,6 +59,7 @@ export async function getStaticProps(context) {
       content: bookData.content,
       category,
       slug,
+      related: relatedBooks,
     },
   };
 }
